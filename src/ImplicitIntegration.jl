9e247e8de2aa8a1b334f081d75294035194f6c5a
Luiz M. Faria <maltezfaria@gmail.com>
2024-09-05 13:57:23 +0200

    first implementation

--- src/ImplicitIntegration.jl
+++ src/ImplicitIntegration.jl
@@ -1,5 +1,186 @@
 module ImplicitIntegration
 
-# Write your package code here.
+import IntervalArithmetic
+import ForwardDiff
+import Roots
+import QuadGK
 
+import StaticArrays: SVector, insert, deleteat
+
+struct HyperRectangle{N,T<:AbstractFloat}
+    lc::SVector{N,T}
+    hc::SVector{N,T}
 end
+
+const Segment{T} = HyperRectangle{1,T}
+
+bounds(rect::HyperRectangle) = (rect.lc, rect.hc)
+
+function remove_dimension(rect::HyperRectangle, k)
+    lc, hc = bounds(rect)
+    return HyperRectangle(deleteat(lc, k), deleteat(hc, k))
+end
+
+function split(U::HyperRectangle, dir)
+    lc, hc = bounds(U)
+    mid = (lc[dir] + hc[dir]) / 2
+    Uₗ = HyperRectangle(lc, setindex(hc, mid, dir))
+    Uᵣ = HyperRectangle(setindex(lc, mid, dir), hc)
+    return Uₗ, Uᵣ
+end
+
+"""
+    bound(f, rec::HyperRectangle)
+
+Return a lower and upper bound for the function `f` on the hypercube `rec`.
+
+When `f` is a scalar function, return a lower and upper bound. When `f` is
+vector valued, return a vector of lower and upper bounds.
+"""
+function bound(f, rec::HyperRectangle{N}) where {N}
+    lc, hc = bounds(rec)
+    I = ntuple(i -> IntervalArithmetic.interval(lc[i], hc[i]), N) |> SVector
+    return IntervalArithmetic.bounds.(f(I))
+end
+
+function restrict(f, U::HyperRectangle{N}, k) where {N}
+    @assert 1 ≤ k ≤ N
+    lc, hc = bounds(U)
+    fl = x̃ -> f(insert(x̃, k, lc[k]))
+    fu = x̃ -> f(insert(x̃, k, hc[k]))
+    return fl, fu
+end
+
+"""
+    struct ImplicitDomain
+
+Structure used to represent a domain `V` as the intersection of several
+implicitly defined domains `Vᵢ`. The domains `Vᵢ` are defined through a set of
+`functions` and `signs` as follows:
+
+- `Vᵢ = {x ∈ U : ϕᵢ(x) > 0}` if `sᵢ = 1`.
+- `Vᵢ = {x ∈ U : ϕᵢ(x) < 0}` if `sᵢ = -1`.
+- `Vᵢ = {x ∈ U : ϕᵢ(x) ≠ 0}` if `sᵢ = 0`.
+"""
+struct ImplicitDomain{N,T}
+    box::HyperRectangle{N,T}
+    functions::Vector{<:Function}
+    flags::Vector{Int}
+end
+
+## Algorithm 1 of Saye 2015
+"""
+    integrand_eval(f, V::ImplicitDomain{N}, a, b, quad1d)
+
+Return a function `f̃ : ℝᴺ⁻¹ -> ℝ` that approximates the one-dimensional
+integral over `I(x̃) ⊂ ℝ` of the function `t -> f(insert(x̃, k, t))`, where the
+integration domain `I` is defined as `I(x̃) = {t ∈ [a,b] :
+sᵢ*ϕᵢ(insert(̃x,k,t) ≥ 0 ∀ (ϕᵢ,sᵢ) ∈ V)}`.
+"""
+function integrand_eval(f, V::ImplicitDomain{N}, dir::Int, quad1d = QuadGK.quadgk) where {N}
+    xl, xu = bounds(V.box)
+    a, b = xl[dir], xu[dir]
+    f̃ = (x̃) -> begin
+        @assert length(x̃) == N - 1 "dimension of x̃ must be $(N - 1)"
+        # compute the connected components
+        bnds = [a, b]
+        for ϕᵢ in V.functions
+            g = (t) -> ϕᵢ(insert(x̃, dir, t))
+            # FIXME: use `IntervalRootFinding` for robustness
+            append!(bnds, Roots.find_zeros(g, a, b))
+        end
+        sort!(bnds)
+        I = 0.0 # FIXME: infer the appropriate type for this accumulator
+        # compute the integral
+        for i in 1:(length(bnds)-1) # loop over each segment
+            rᵢ, rᵢ₊₁ = bnds[i], bnds[i+1]
+            L = rᵢ₊₁ - rᵢ
+            # decide if the segment is inside the domain
+            xc = insert(x̃, dir, (rᵢ + rᵢ₊₁) / 2)
+            skip = false
+            for (ϕᵢ, sᵢ) in zip(V.functions, V.flags)
+                sᵢ == 0 && continue # avoid evaluation of ϕᵢ(xc) when possible
+                if sᵢ * ϕᵢ(xc) < 0
+                    skip = true
+                    break
+                end
+            end
+            skip && continue
+            # add the contribution of the segment by performing a 1D quadrature
+            I += quad1d(rᵢ, rᵢ₊₁) do t
+                x = insert(x̃, dir, t)
+                return f(x)
+            end
+        end
+        return I
+    end
+    return f̃
+end
+
+function integrate(f, Ω::ImplicitDomain{DIM,T}, quad1d) where {DIM,T}
+    U = Ω.box
+    xl, xu = bounds(U)
+    xc = (xl + xu) / 2
+    ϕ, s = Ω.functions, Ω.flags
+    n = length(ϕ)
+    if DIM == 1
+        f̃ = integrand_eval(f, Ω, 1, quad1d)
+        x̃ = SVector{0,T}() # TODO: this is a hack to make `integrand_eval` work for N=1
+        return f̃(x̃)
+    else
+        # TODO: add prunning stage to remove trivial ϕᵢ (whole box or empty box)
+        # TODO: tensor product quadrature when prunning removes all ϕᵢ
+        ∇ϕ₁ = (x) -> ForwardDiff.gradient(ϕ[1], x)
+        k = argmax(∇ϕ₁(xc))
+        ϕ̃, s̃ = Function[], empty(s)
+        for i in 1:n
+            ∇ϕᵢ = (x) -> ForwardDiff.gradient(ϕ[i], x)
+            ∇ϕᵢ_bnds = bound(∇ϕᵢ, U)
+            den = sum(∇ϕᵢ_bnds) do (lb, ub)
+                return max(abs(lb), abs(ub))^2
+            end |> sqrt # max over U of |∇ϕᵢ|
+            lb, ub = ∇ϕᵢ_bnds[k]
+            min_qual = 0
+            qual = den == 0 ? 1.0 : lb * ub > 0 ? min(abs(lb), abs(ub)) / den : 0.0 # quality factor
+            if qual > min_qual
+                ϕᵢᴸ, ϕᵢᵁ = restrict(ϕ[i], U, k)
+                sᵢᴸ, sᵢᵁ = sgn(sign(lb), s[i], false, -1), sgn(sign(lb), s[i], false, 1)
+                push!(ϕ̃, ϕᵢᴸ, ϕᵢᵁ)
+                push!(s̃, sᵢᴸ, sᵢᵁ)
+            else
+                # split along largest direction
+                # TODO: stop subdivision if U is too small and return midpoint
+                Uₗ, Uᵣ = split(U, argmax(xu - xl))
+                Ωₗ, Ωᵣ = ImplicitDomain(Uₗ, ϕ, s), ImplicitDomain(Uᵣ, ϕ, s)
+                return integrate(f, Ωₗ, quad1d) + integrate(f, Ωᵣ, quad1d)
+            end
+        end
+        # If we got here then we have a good direction k do recurse down on the
+        # dimension
+        Ũ = remove_dimension(U, k)
+        f̃ = integrand_eval(f, Ω, k, quad1d)
+        Ω̃ = ImplicitDomain(Ũ, ϕ̃, s̃)
+        return integrate(f̃, Ω̃, quad1d)
+    end
+end
+
+"""
+    sgn(m, s, S::Bool, σ)
+
+Helper function to compute the sign of lower and upper restrictions of a
+level-set function `ϕᵢ` in a box along a given height direction `k`. Here `m` is
+sign of `∂ₖϕᵢ`, which is assume not to change throughout the box since `k` is
+assumed to be a height direction, s is the sign of `ϕᵢ` in the box, S is a flag
+to indicate whether we are in the special case of a surface integral.
+"""
+function sgn(m, s, S::Bool, σ)
+    if (m == σ * s) || S
+        return σ * m
+    else
+        return 0
+    end
+end
+
+# export bound, restrict
+
+end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,10 +1,52 @@
-using ImplicitIntegration
-using Test
 using Aqua
+using ImplicitIntegration
+using StaticArrays
+using ForwardDiff
+using Test
 
 @testset "ImplicitIntegration.jl" begin
     @testset "Code quality (Aqua.jl)" begin
         Aqua.test_all(ImplicitIntegration)
     end
-    # Write your tests here.
+
+    @testset "Bound with IntervalArithmetic" begin
+        # Utility function to test the bound function
+        function test_bound(f, lc, hc, nsamples = 100^length(lc))
+            N = length(lc)
+            ∇f = (x) -> ForwardDiff.gradient(f, x)
+            f_bnd = ImplicitIntegration.bound(f, lc, hc)
+            ∇f_bnds = ImplicitIntegration.bound(∇f, lc, hc)
+            for _ in 1:nsamples
+                x = lc .+ (hc .- lc) .* ntuple(i -> rand(), N)
+                f_bnd[1] <= f(x) <= f_bnd[2] || (return false)
+                grad = ∇f(x)
+                all(i -> ∇f_bnds[i][1] ≤ grad[i] ≤ ∇f_bnds[i][2], 1:N) || (return false)
+            end
+            return true
+        end
+        ## 1D
+        f = x -> x[1]^3 * sin(x[1])
+        lc, hc = SVector(0.0), SVector(2.0)
+        @test test_bound(f, lc, hc)
+        @inferred ImplicitIntegration.bound(f, lc, hc)
+        ## 2D
+        f = x -> x[1]^2 + x[2]^2
+        lc, hc = SVector(0.0, 0.0), SVector(1.0, 1.0)
+        @test test_bound(f, lc, hc)
+        @inferred ImplicitIntegration.bound(f, lc, hc)
+        ## 3D
+        f = x -> x[1]^2 + x[2]^2 + x[3]^2
+        lc, hc = SVector(0.0, 0.0, 0.0), SVector(1.0, 1.0, 1.0)
+        @test test_bound(f, lc, hc)
+        @inferred ImplicitIntegration.bound(f, lc, hc)
+    end
+
+    @testset "Restrict" begin
+        f = (x) -> x[1]^2 + x[2]^2
+        k = 1
+        lc, hc = SVector(0.0, 0.0), SVector(1.0, 1.0)
+        fL, fU = restrict(f, lc, hc, k)
+        x̃ = SVector(0.5)
+        fL(x̃)
+    end
 end
